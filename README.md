fd6be384d31878f2fd71a023802178a1793dea47
WooHyuk Jang <morrranii@gmail.com>
2021-03-19 20:19:04 +0900

    Count Number of Mutations
    Added the 1_onehot_mutations.py, which analyzes where snps occur.
    The snp occurence data is saved in the mutations.csv file. Also edited
    README file.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# bioinformatics_study
-과제연구 관련 코드를 모아놓는 레포지터리이다.
+## Identification of SNPs in the SARS-CoV-2 Spike Glycoprotein
+We anaylzed variants in 40939 SARS-CoV-2 Spike protein amino acid sequences, and created a random forest classifier, predicting the sequence's location.
+
+## Languages and Environments
+We used python to preprocess our data, and R to build the random forest.
+The scripts were written and executed via VSCode and RStudio in a conda environment.
--- scripts/0_check_data.py
+++ scripts/0_check_data.py
@@ -1,23 +1,24 @@
+'''
+Just checking whether the data is correct. 
+The data should have 40939 aligned sequences, respectively lengthed 1273 amino acids.
+'''
+
 from Bio import SeqIO
+from Bio import AlignIO
 from Bio import pairwise2
 from Bio.SeqRecord import SeqRecord
 
-ref_seq = ()
-sequences = ()
-
 # open files
-ref_file = open("bioinformatics_study/data/before_preprocessing/refseq.fasta", "r", encoding='utf-8')
-seq_file = open("bioinformatics_study/data/before_preprocessing/sequences.fasta", "r", encoding='utf-8')
-align_file = open("bioinformatics_study/data/after_preprocessing/alignments.fas", "w", encoding='utf-8')
+ref_file = open("data/before_preprocessing/refseq.fasta", "r", encoding='utf-8')
+align_file = open("data/before_preprocessing/alignments.fasta", "r", encoding='utf-8')
 
 # read files
 ref_seq = SeqIO.read(ref_file, "fasta")
-sequences = SeqIO.parse(seq_file, "fasta")
+alignments = AlignIO.parse(align_file, "fasta")
 
-print(len(ref_seq))
-print(len(list(sequences)))
+print("length of each sequence:", len(ref_seq))
+print("number of aligned sequences:", len(list(alignments)[0]))
 
 # close files
 ref_file.close()
-seq_file.close()
 align_file.close()
